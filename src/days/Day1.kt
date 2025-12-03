6ea63c2af6c72400ae5e7ddf5df95d98f784e8f8
Nico Thomas Beranek <nico.thomas.beranek@twinformatics.at>
2025-12-03 19:35:05 +0100

    day 1 wip

--- src/days/Day1.kt
+++ src/days/Day1.kt
@@ -7,11 +7,88 @@ import kotlinx.datetime.LocalDate
 object Day1 : AoCDay {
     override val day: LocalDate = day(1)
 
-    override fun executePart1(input: String): Any {
-        return "(not implemented)"
+    private enum class Direction constructor(directionKey: Char) {
+        LEFT('L'),
+        RIGHT('R');
+
+        companion object {
+            fun ofChar(directionKey: Char) = when (directionKey) {
+                'L' -> LEFT
+                'R' -> RIGHT
+                else -> throw IllegalArgumentException("Unknown direction key: $directionKey")
+            }
+        }
     }
 
+    override fun executePart1(input: String): Any {
+        var result = 0
+        var safeDail = Dail()
+        input.lines().forEach { line ->
+            safeDail = when (val moveDirection = Direction.ofChar(line.first())) {
+                Direction.RIGHT -> safeDail.turn(line.substring(1).toInt(), moveDirection)
+                Direction.LEFT -> safeDail.turn(line.substring(1).toInt(), moveDirection)
+            }
+            if (safeDail.current == 0) result += 1
+        }
+
+        return result
+    }
+
+    private data class Dail(
+        private val min: Int = 0,
+        val current: Int = 50,
+        private val max: Int = 99,
+        var turnover: Int = 0
+    ) {
+
+
+        fun turn(count: Int, direction: Direction): Dail {
+            return when(direction) {
+                Direction.LEFT -> {
+                    if(current - count < min) {
+                        var foo = current
+                        while(foo - count < min) {
+                            foo += 99
+                            if(foo - count == 0) turnover += 1
+                            turnover += 1
+                        }
+                        this.copy(current = (current - count) % 100, turnover = turnover)
+                    }
+                    else {
+                        if(current - count == 0) turnover += 1
+                        this.copy(current = current - count, turnover = turnover)
+                    }
+                }
+
+                Direction.RIGHT -> {
+                    if(current + count > max) {
+                        var foo = current
+                        while(foo + count > max) {
+                            foo -= 99
+                            if(foo + count == 0) turnover += 1
+                            turnover += 1
+                        }
+                        this.copy(current = (current + count) % 100, turnover = turnover)
+                    }
+                    else {
+                        if(current + count == 0) turnover += 1
+                        this.copy(current = current + count, turnover = turnover)
+                    }
+                }
+            }
+        }
+    }
+
+
     override fun executePart2(input: String): Any {
-        return "(not implemented)"
+        var safeDail = Dail()
+        input.lines().forEach { line ->
+            safeDail = when (val moveDirection = Direction.ofChar(line.first())) {
+                Direction.RIGHT -> safeDail.turn(line.substring(1).toInt(), moveDirection)
+                Direction.LEFT -> safeDail.turn(line.substring(1).toInt(), moveDirection)
+            }
+        }
+
+        return safeDail.turnover
     }
 }
--- src/main.kt
+++ src/main.kt
@@ -3,6 +3,6 @@ package com.r3tro04
 import com.r3tro04.structure.AoC
 
 fun main() {
-    println(AoC.executeSimpleToday().formatResult())
-    AoC.printAllTimed(100, 10)
+    println(AoC.executeSimple(1).formatResult())
+    //AoC.printAllTimed(100, 10)
 }
